172d62b3d3a6e630881f11e3b52d6d946e918074
Hazem <ihazem28@gmail.com>
2025-11-25 17:32:11 +0200

    Added AddbookActivity and BookDetailActivity

---
+++ app/src/main/java/com/example/booktrackerapp/AddBookActivity.kt
@@ -0,0 +1,13 @@
+package com.example.booktrackerapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AddBookActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
